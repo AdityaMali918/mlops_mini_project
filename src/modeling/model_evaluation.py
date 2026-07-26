74e857f45d078f75798827688dedfdfd4989e037
Aditya <adityamali918@gmail.com>
2026-07-26 15:57:01 +0530

    modev_eval artifact name changes

--- src/modeling/model_evaluation.py
+++ src/modeling/model_evaluation.py
@@ -148,7 +148,11 @@ def main():
                     mlflow.log_param(param_name, param_value)
             
             # Log model to MLflow
-            mlflow.sklearn.log_model(clf, "model")
+            # mlflow.sklearn.log_model(clf, "model")
+            mlflow.sklearn.log_model(
+                sk_model=clf,
+                name="model"
+            )
             
             # Save model info
             save_model_info(run.info.run_id, "model", 'reports/experiment_info.json')
